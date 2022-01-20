fe2eb9850e5118fc2816279329a5ecf057163a86
Jorge Dorio <dorio@pop-os.localdomain>
2022-01-20 03:03:08 -0400

    Arrumando lint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
-import Search from './pages/Search';
-import Album from './pages/Album';
-import Favorites from './pages/Favorites';
-import Profile from './pages/Profile';
-import ProfileEdit from './pages/ProfileEdit';
-import Login from './pages/Login';
-import NotFound from './pages/NotFound';
 import Header from './components/Header';
 import Content from './components/Content';
 
@@ -17,20 +10,6 @@ class App extends React.Component {
       <BrowserRouter>
         <Header />
         <Content />
-
-        {/* <Switch>
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile" component={ Profile } />
-          <Route
-            path="/profile/edit"
-            render={ (props) => (
-              <ProfileEdit { ...props } />) }
-          />
-          <Route path="/" component={ Login } />
-          <Route path="*" component={ NotFound } />
-        </Switch> */}
       </BrowserRouter>
     );
   }
