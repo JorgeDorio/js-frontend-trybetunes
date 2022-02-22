51fe5fece1fb6fbf24bb27eb299f373459278980
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-02-22 01:14:26 -0400

    5. Crie o formulário para pesquisa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -27,9 +28,19 @@ export default class Header extends Component {
 
   render() {
     const { loading, userName } = this.state;
+    const headerDiv = (
+      <div>
+        <h1 data-testid="header-user-name">{userName}</h1>
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Buscar</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Músicas Favoritas</Link>
+          <Link to="/profile" data-testid="link-to-profile">Meu Perfil</Link>
+        </nav>
+      </div>
+    );
     return (
       <header data-testid="header-component">
-        { loading ? <Loading /> : <h1 data-testid="header-user-name">{userName}</h1>}
+        { loading ? <Loading /> : headerDiv}
       </header>
     );
   }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,13 +1,64 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 
+const minLengthName = 2;
+
 export default class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      btnIsDisabled: true,
+      nameSearched: '',
+    };
+  }
+
+  checkInput = () => {
+    const { nameSearched } = this.state;
+
+    if (nameSearched.length >= minLengthName) {
+      this.setState({
+        btnIsDisabled: false,
+      });
+    } else {
+      this.setState({
+        btnIsDisabled: true,
+      });
+    }
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, this.checkInput);
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+  }
+
   render() {
+    const { nameSearched, btnIsDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form onSubmit={ this.handleSubmit }>
+          <input
+            type="text"
+            placeholder="Nome da banda ou Artista"
+            data-testid="search-artist-input"
+            value={ nameSearched }
+            name="nameSearched"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ btnIsDisabled }
+          >
+            Pesquisar
 
+          </button>
+        </form>
       </div>
     );
   }
