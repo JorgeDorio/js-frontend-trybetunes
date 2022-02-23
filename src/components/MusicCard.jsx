e777385559a53c73bebce7b227ac54cd6c922157
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-02-22 22:45:03 -0400

    8 e 9. Crie o mecanismo para adicionar aos favoritos

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,94 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
+
+export default class MusicCard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+    };
+  }
+
+  addToFavorite = async (music) => {
+    const { getFavSongs } = this.props;
+    this.setState({
+      loading: true,
+    });
+    addSong(music).then(() => {
+      this.setState({
+        loading: false,
+      });
+    });
+    getFavSongs();
+  };
+
+  render() {
+    const { findedMusics, renderMusics, favMusics } = this.props;
+    const { loading } = this.state;
+    const card = (
+      <div>
+        {renderMusics && findedMusics.length > 0 ? (
+          <div>
+            <h2 data-testid="artist-name">{findedMusics[0].artistName}</h2>
+            <h3 data-testid="album-name">{findedMusics[0].collectionName}</h3>
+            <ul>
+              {findedMusics.slice(1).map((music) => (
+                <li key={ music.trackId }>
+                  <div>
+                    {music.trackName}
+                    <audio
+                      data-testid="audio-component"
+                      src={ music.previewUrl }
+                      controls
+                    >
+                      <track kind="captions" />
+                      O seu navegador não suporta o
+                      elemento
+                      {' '}
+                      <code>audio</code>
+                    </audio>
+                    <label
+                      htmlFor={ music.trackId }
+                      data-testid={ `checkbox-music-${music.trackId}` }
+                    >
+                      {' '}
+                      <input
+                        type="checkbox"
+                        name={ music.trackId }
+                        id={ music.trackId }
+                        onChange={ () => {
+                          this.addToFavorite(music);
+                        } }
+                        checked={
+                          favMusics.filter(
+                            (filtermusic) => filtermusic.trackId === music.trackId,
+                          ).length > 0
+                        }
+                      />
+                      {' '}
+                      Favorita
+                    </label>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>Não rederizou</p>
+        )}
+      </div>
+    );
+    return <div>{loading ? <Loading /> : card}</div>;
+  }
+}
+
+MusicCard.propTypes = {
+  findedMusics: PropTypes.shape({
+    length: PropTypes.number,
+    slice: PropTypes.func,
+  }),
+  renderMusics: PropTypes.any,
+}.isRequired;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 import getMusics from '../services/musicsAPI';
 
 export default class Album extends Component {
@@ -12,11 +14,13 @@ export default class Album extends Component {
       loading: true,
       findedMusics: [],
       renderMusics: false,
+      favMusics: [],
     };
   }
 
   componentDidMount() {
     this.requestMusics();
+    this.getFavSongs();
   }
 
   requestMusics = async () => {
@@ -32,60 +36,32 @@ export default class Album extends Component {
     });
   };
 
+  getFavSongs = async () => {
+    getFavoriteSongs().then((music) => {
+      this.setState({
+        loading: false,
+        favMusics: music,
+      });
+    });
+  };
+
   render() {
-    const { loading, findedMusics, renderMusics } = this.state;
-    // console.log(typeof(findedMusics[0]));
-    // const teste = (<div>
-    //   <h2 data-testid="artist-name">{findedMusics[0].artistName}</h2>
-    //   <h3 data-testid="album-name">{findedMusics[0].collectionName}</h3>
-    //   <ul>
-    //       {findedMusics.map((music) => (<li key={ music.trackId }>
-    //       <div>
-    //           {music.trackName}
-    //           <audio controls="controls">
-    //           <source src={ music.previewUrl } type="audio/mp3" />
-    //         </audio>
-    //         </div>
-    //       </li>))}
-    //     </ul>
-    // </div>)
-    const albumDiv = (
-      <div>
-        {renderMusics && findedMusics.length > 0 ? (
-          <div>
-            <h2 data-testid="artist-name">{findedMusics[0].artistName}</h2>
-            <h3 data-testid="album-name">{findedMusics[0].collectionName}</h3>
-            <ul>
-              {findedMusics.slice(1).map((music) => (
-                <li key={ music.trackId }>
-                  <div>
-                    {music.trackName}
-                    <audio
-                      data-testid="audio-component"
-                      src={ music.previewUrl }
-                      controls
-                    >
-                      <track kind="captions" />
-                      O seu navegador não suporta o
-                      elemento
-                      {' '}
-                      <code>audio</code>
-                      .
-                    </audio>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ) : (
-          <p>Não rederizou</p>
-        )}
-      </div>
-    );
+    const { loading, findedMusics, renderMusics, favMusics } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        <div>{loading ? <Loading /> : albumDiv}</div>
+        <div>
+          {loading ? (
+            <Loading />
+          ) : (
+            <MusicCard
+              getFavSongs={ this.getFavSongs }
+              renderMusics={ renderMusics }
+              findedMusics={ findedMusics }
+              favMusics={ favMusics }
+            />
+          )}
+        </div>
       </div>
     );
   }
