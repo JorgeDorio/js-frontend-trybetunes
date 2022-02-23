dd1cde8b037fab6468c6456599fd2ddbdb38e96f
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-02-23 01:41:41 -0400

    13. Crie a exibição de perfil

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,6 +14,9 @@ export default class Login extends Component {
       btnIsDisabled: true,
       loading: false,
       isRedirect: false,
+      loginEmail: '',
+      loginImage: '',
+      loginDescription: '',
     };
   }
 
@@ -29,27 +32,43 @@ export default class Login extends Component {
         btnIsDisabled: true,
       });
     }
-  }
+  };
 
   handleChange = ({ target: { name, value } }) => {
-    this.setState({
-      [name]: value,
-    }, this.checkInput);
-  }
+    this.setState(
+      {
+        [name]: value,
+      },
+      this.checkInput,
+    );
+  };
 
   handleSubmit = async (e) => {
     e.preventDefault();
 
     this.setState({ loading: true });
-    const { loginName } = this.state;
-    await createUser({ name: loginName });
+    const { loginName, loginEmail, loginImage, loginDescription } = this.state;
+    await createUser({
+      name: loginName,
+      email: loginEmail,
+      image: loginImage,
+      description: loginDescription,
+    });
     this.setState({
       isRedirect: true,
     });
-  }
+  };
 
   render() {
-    const { loading, isRedirect, loginName, btnIsDisabled } = this.state;
+    const {
+      loading,
+      isRedirect,
+      loginName,
+      btnIsDisabled,
+      loginEmail,
+      loginDescription,
+      loginImage,
+    } = this.state;
     const loginDiv = (
       <div data-testid="page-login">
         <form onSubmit={ this.handleSubmit }>
@@ -61,21 +80,40 @@ export default class Login extends Component {
             name="loginName"
             placeholder="Insira seu nome"
           />
+          <input
+            type="email"
+            onChange={ this.handleChange }
+            value={ loginEmail }
+            name="loginEmail"
+            placeholder="Insira seu email"
+          />
+          <textarea
+            onChange={ this.handleChange }
+            value={ loginDescription }
+            name="loginDescription"
+            placeholder="Insira sua descrição"
+          />
+          <input
+            type="text"
+            onChange={ this.handleChange }
+            value={ loginImage }
+            name="loginImage"
+            placeholder="Src da imagem"
+          />
           <button
             disabled={ btnIsDisabled }
             type="submit"
             data-testid="login-submit-button"
           >
             Entrar
-
           </button>
         </form>
       </div>
     );
     return (
       <div>
-        { loading ? <Loading /> : loginDiv }
-        { isRedirect && <Redirect to="/search" /> }
+        {loading ? <Loading /> : loginDiv}
+        {isRedirect && <Redirect to="/search" />}
       </div>
     );
   }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 export default class Profile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userName: '',
+      userEmail: '',
+      loginDescription: '',
+      loginImage: '',
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.requestUser();
+  }
+
+  requestUser = async () => {
+    getUser().then(({ name, email, description, image }) => {
+      this.setState({
+        loading: false,
+        userName: name,
+        userEmail: email,
+        loginImage: image,
+        loginDescription: description,
+      });
+    });
+  };
+
   render() {
+    const { loading, userName, userEmail, loginDescription, loginImage } = this.state;
+    const profileDiv = (
+      <div>
+        <p>{userName}</p>
+        <p>{userEmail}</p>
+        <p>{loginDescription}</p>
+        <img
+          src={ loginImage }
+          alt="Profile"
+          data-testid="profile-image"
+        />
+        <Link to="/profile/edit">Editar perfil</Link>
+      </div>
+    );
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
-
+        {loading ? <Loading /> : profileDiv}
       </div>
     );
   }
